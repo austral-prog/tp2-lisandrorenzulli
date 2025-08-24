630c81cf24c11d3408d34cb14465270b00d0fa89
lisandrorenzulli <lisandrorenzulli@gmail.com>
2025-08-24 20:20:47 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,3 +1,16 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = money - expense
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print(int(vuelto))  
+    print("Centavos:")
+    print(int(round((vuelto - int(vuelto)) * 100))) 
+change()
